20ad00f2bfa721a8d2dfaf49c0c791f577532fc3
ecologic informatic / 404A Studio <109870720+ecologicinformaticYT@users.noreply.github.com>
2024-09-15 11:29:51 +0200

    Update cmd-converter.js

--- js/cmd-converter.js
+++ js/cmd-converter.js
@@ -144,3 +144,19 @@ setInterval(function() {
     console.log(result);
 }, 1000);
 
+
+// Fonction pour vérifier les dimensions de l'écran
+function adjustBodyId() {
+    const body = document.body;
+    if (window.innerWidth > window.innerHeight) {
+        body.id = 'desktop'; // Ajoute l'ID 'desktop' si la largeur est supérieure à la hauteur
+    } else {
+        body.id = 'mobile'; // Supprime l'ID si la condition n'est pas remplie
+    }
+}
+
+// Appelle la fonction lors du chargement de la page
+window.onload = adjustBodyId;
+
+// Appelle la fonction lors du redimensionnement de la fenêtre
+window.onresize = adjustBodyId;
