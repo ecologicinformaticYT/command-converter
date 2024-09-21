f16a2674dede6a172d29f74b96fe3d9b5907933f
Nitnaf10 <fantin6000@gmail.com>
2024-09-21 11:12:45 +0200

    Update cmd-converter.js

--- js/cmd-converter.js
+++ js/cmd-converter.js
@@ -143,20 +143,3 @@ setInterval(function() {
     let result = convert_command(input);
     console.log(result);
 }, 1000);
-
-
-// Fonction pour vérifier les dimensions de l'écran
-function adjustBodyId() {
-    const body = document.body;
-    if (window.innerWidth > window.innerHeight) {
-        body.id = 'desktop'; // Ajoute l'ID 'desktop' si la largeur est supérieure à la hauteur
-    } else {
-        body.id = 'mobile'; // Supprime l'ID si la condition n'est pas remplie
-    }
-}
-
-// Appelle la fonction lors du chargement de la page
-window.onload = adjustBodyId;
-
-// Appelle la fonction lors du redimensionnement de la fenêtre
-window.onresize = adjustBodyId;
